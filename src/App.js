12a6c9a2fe313d5b424f64781f81f99cee36c71d
dienigma <joshi.chinmay09@gmail.com>
2019-06-29 17:11:51 +0530

    Built a basic code server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initCode = `function add (a,b){
+    // Please fill code here
+};`;
+
+class App extends React.Component {
+  state = { code: initCode };
+  resetCode = () => {
+    this.setState({ code: initCode });
+  };
+  render() {
+    return (
+      <div>
+        <Editor
+          value={this.state.code}
+          onValueChange={code => this.setState({ code })}
+          highlight={code => highlight(code, languages.js)}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 20
+          }}
+        />
+        <button onClick={this.resetCode}>Reset</button>
+        <button />
+      </div>
+    );
+  }
 }
 
 export default App;
